f4e9f054a6ff4e738d9bcd7aaed08080874a9d1b
youssef khemiri <youssefkhemiri722@gmail.com>
2022-06-27 11:30:14 +0100

    children v1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import './App.css';
 import { Component } from 'react';
-
+import { CardList } from './components/card-list/card-list';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'frank',
-          id: 'a'
-        },
-        {
-          name: 'frank2',
-          id: 'a2'
-        },
-        {
-          name: 'frank3',
-          id: 'a3'
-        }
-      ]
+      monsters: []
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+
 
   render() {
       return (
         <div className="App">
+          <CardList name='ysf'>
           {this.state.monsters.map(monster => (
             <h1 key={monster.id}> {monster.name}</h1>
           ))}
+          </CardList>
         </div>
       );
     } 
